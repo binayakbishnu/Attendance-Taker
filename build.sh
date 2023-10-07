d333381ba3c465a17b1dd594675be03248d3f639
Binayak Bishnu <bishnu.binayak12@gmail.com>
2023-10-07 23:27:04 +0530

    streamlit solution implemented

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -o errexit
 
 pip install --upgrade pip
 pip install wheel
-pip install dlib
+# pip install TA-Lib
+# pip install dlib
 # pip install dlib-19.22.99-cp310-cp310-win_amd64.whl
-pip install face_recognition
+# pip install face_recognition
+pip install git+https://github.com/binayakbishnu/face_recognition.git
 pip install -r req.txt
 
 python manage.py core --no-input
