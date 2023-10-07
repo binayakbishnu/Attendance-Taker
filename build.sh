3bf295db7ea629b3c1c30c0415f1b225263cf1d9
Binayak Bishnu <bishnu.binayak12@gmail.com>
2023-10-07 22:44:16 +0530

    install dlib in cmdline direct

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -o errexit
 
 pip install --upgrade pip
+pip install wheel
+pip install dlib
 # pip install dlib-19.22.99-cp310-cp310-win_amd64.whl
 pip install face_recognition
-# pip install wheel
 pip install -r req.txt
 
 python manage.py core --no-input
