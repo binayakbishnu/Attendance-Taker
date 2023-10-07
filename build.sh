21fc20e82f250f02a8f3aadf4c2bec339405a962
Binayak Bishnu <bishnu.binayak12@gmail.com>
2023-10-07 22:25:50 +0530

    upgrading pip in build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 # pip install dlib-19.22.99-cp310-cp310-win_amd64.whl
 pip install face_recognition
+# pip install wheel
 pip install -r req.txt
 
 python manage.py core --no-input
